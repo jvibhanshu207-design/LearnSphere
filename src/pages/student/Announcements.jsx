be044b337e13f531f65edd7706ba48e54cfcd4e3
Jayant <kjayant808@gmail.com>
2026-09-13 18:46:01 +0530

    changes

--- src/pages/student/Announcements.jsx
+++ src/pages/student/Announcements.jsx
@@ -1,34 +1,3 @@
-// import React, { useState } from "react";
-// import { initialAnnouncements } from "../../data/announcements";
-// import AnnouncementCard from "../../components/AnnouncementCard";
-// import "../../styles/pages/Announcements.css";
-
-// export const Announcements = () => {
-//   const [announcements] = useState(initialAnnouncements);
-
-//   return (
-//     <div className="announcements-page">
-//       {/* Header */}
-//       <div>
-//         <h2 className="announcements-title">Announcements</h2>
-//         <p className="announcements-desc">
-//           Stay updated with high priority announcements from your instructors.
-//         </p>
-//       </div>
-
-//       {/* Grid */}
-//       <div className="announcements-list">
-//         {announcements.map((item) => (
-//           <AnnouncementCard key={item.id} announcement={item} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Announcements;
-
-
 const Announcements = () => {
     return (
         <div className="under-construction">
